d58c603ebd9c402a4ca730819bff2cc9e60d8b15
Stephane Roy <stephroy411@hotmail.com>
2021-08-17 13:46:25 -0300

    Add function operate that takes an operator and 2 numbers and then calls one of the operator functions on the number

--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,35 @@
 function add(a, b) {
     return a + b
-  }
+  };
   
   function subtract(a, b) {
     return a - b
-  }
+  };
   
   function multiply(a, b) {
     return a * b
-  }
+  };
   
   function divide(a, b) {
     return a / b
-  }
+  };
   
 
-  console.log(divide(3,2));
+  function operate(operator, num1, num2) {
+    switch (operator) {
+      case '+':
+        return add(num1, num2)
+      case '−':
+        return substract(num1, num2)
+      case '×':
+        return multiply(num1, num2)
+      case '÷':
+        if (num2 === 0) 
+        return null
+        else 
+        return divide(num1, num2)
+      default:
+        return null
+    }
+  };
+
