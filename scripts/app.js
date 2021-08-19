44da694d894f8eb1d4fa781277f8ee7212a34b5a
Stephane Roy <stephroy411@hotmail.com>
2021-08-19 14:29:07 -0300

    Add data types in index.html and added logic for calculator in app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,36 +1,68 @@
+class Calculator {
+  constructor(previousOperandText, currentOperandText) {
+    this.previousOperandText = previousOperandText
+    this.currentOperandText = currentOperandText
+    this.clear()
+  }
+
+  clear() {
+    this.currentOperand = ""
+    this.previousOperand = ""
+    this.operation = undefined
+  }
+
+  delete() {
+
+  }
+
+  appendNumber(number) {
+    if (number === "." && this.currentOperand.includes(".")) 
+    return 
+    this.currentOperand = this.currentOperand.toString() + number.toString()
+  }
+
+  chooseOperation(operation) {
+    if (this.currentOperandText === "") return
+    if (this.previousOperand !== "") {
+      this.compute()
+    }
+    this.operation = operation
+    this.previousOperand = this.currentOperand
+    this.currentOperand = ""
+  }
+
+  compute() {
+
+  }
+
+  updateDisplay() {
+    this.currentOperandText.innerText = this.currentOperand
+    this.previousOperandText.innerText = this.previousOperand
+  }
+}
 
 
-function add(a, b) {
-  return a + b
-};
 
-function subtract(a, b) {
-  return a - b
-};
+const numberButtons = document.querySelectorAll("[data-number]");
+const operationButtons = document.querySelectorAll("[data-operation]");
+const equalsButton = document.querySelector("[data-equals]");
+const deleteButton = document.querySelector("[data-delete]");
+const allClearButton = document.querySelector("[data-all-clear]");
+const previousOperandText = document.querySelector("[data-previous-operand]");
+const currentOperandText = document.querySelector("[data-current-operand]");
 
-function multiply(a, b) {
-  return a * b
-};
+const calculator = new Calculator(previousOperandText, currentOperandText);
 
-function divide(a, b) {
-  return a / b
-};
+numberButtons.forEach(button => {
+  button.addEventListener("click", () => {
+    calculator.appendNumber(button.innerText)
+    calculator.updateDisplay()
+  })
+})
 
-function operate(operator, num1, num2) {
-  switch (operator) {
-    case '+':
-    return add(num1, num2)
-    case '−':
-    return substract(num1, num2)
-    case '×':
-    return multiply(num1, num2)
-    case '÷':
-    if (num2 === 0) 
-    return null
-    else 
-    return divide(num1, num2)
-    default:
-    return null
- }
-};
-
+operationButtons.forEach(button => {
+  button.addEventListener("click", () => {
+    calculator.chooseOperation(button.innerText)
+    calculator.updateDisplay()
+  })
+})
